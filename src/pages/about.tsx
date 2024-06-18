35398f2cb1e7d86c63ce21c465e6971c5d75f360
ohara <kzAme79@gmail.com>
2024-06-18 22:30:20 +0900

    add pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <>
+      <div>使い方</div>
+    </>
+  )
+}
+
+export default About
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const Home = () => {
+  return (
+    <>
+      <div>Pokemon builder 2</div>
+      <Link href="/about">About</Link>
+    </>
+  )
+}
+
+export default Home
