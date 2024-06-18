7d64b338f8abea6561ddbb01b8fe8e662c92d837
ohara <kzAme79@gmail.com>
2024-06-18 22:55:16 +0900

    add tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import type { AppProps } from "next/app"
 import Layout from "@/components/Layout"
+import "@/app/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
