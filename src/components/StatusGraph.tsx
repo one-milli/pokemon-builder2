44a522cbdaf9cffba3650a08f6867db96d1e3e5a
Kazuma Ohara <kzAme79@gmail.com>
2024-07-11 16:03:06 +0900

    Update StatusGraph.tsx

--- src/components/StatusGraph.tsx
+++ src/components/StatusGraph.tsx
@@ -21,14 +21,14 @@ const StatusGraph = ({ pokemon }: Props) => {
     { subject: "HP", realValue: 300, effortValue: 150 },
     { subject: "攻撃", realValue: 250, effortValue: 200 },
     { subject: "防御", realValue: 200, effortValue: 100 },
-    { subject: "特攻", realValue: 350, effortValue: 252 },
-    { subject: "特防", realValue: 180, effortValue: 50 },
     { subject: "素早さ", realValue: 280, effortValue: 180 },
+    { subject: "特防", realValue: 180, effortValue: 50 },
+    { subject: "特攻", realValue: 350, effortValue: 252 },
   ]
 
   return (
     <>
-      <div className="w-full max-w-xl mx-auto p-4 border border-gray-300 rounded-lg">
+      <div className="w-full max-w-xl mx-auto p-4">
         <div className="flex items-center space-x-2 mb-4">
           <Switch
             id="effort-values"
@@ -37,18 +37,17 @@ const StatusGraph = ({ pokemon }: Props) => {
           />
           <Label htmlFor="effort-values">努力値の表示</Label>
         </div>
-        <div className="w-full h-[400px] bg-gray-100">
+        <div className="w-full h-[200px]">
           <RadarChart
-            cx={200}
-            cy={200}
-            outerRadius={150}
-            width={400}
-            height={400}
+            cx={100}
+            cy={80}
+            outerRadius={60}
+            width={300}
+            height={300}
             data={data}
           >
             <PolarGrid />
             <PolarAngleAxis dataKey="subject" />
-            <PolarRadiusAxis angle={30} domain={[0, 600]} />
             <Radar
               name="実数値"
               dataKey="realValue"
@@ -67,16 +66,6 @@ const StatusGraph = ({ pokemon }: Props) => {
             )}
             <Legend />
           </RadarChart>
-        </div>
-        <div className="mt-4">
-          {data.map((entry, index) => (
-            <div key={`value-${index}`} className="flex justify-between">
-              <span>{entry.subject}</span>
-              <span>
-                実数値: {entry.realValue}, 努力値: {entry.effortValue}
-              </span>
-            </div>
-          ))}
         </div>
       </div>
     </>
